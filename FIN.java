ae4f4f7aab1113459abc46911ab7fd6c3042d898
Keshav <110564015+keshavsolankii@users.noreply.github.com>
2023-07-19 07:47:33 +0530

    Update FIN.java

--- FIN.java
+++ FIN.java
@@ -20,3 +20,37 @@ public class FIN {
         
     }
 }
+// BufferedInputStream example
+class BIS{
+    public static void main(String[] args) {
+        try {
+            FileInputStream fis = new FileInputStream("E:\\Java IO\\temp.txt");
+            BufferedInputStream bis = new BufferedInputStream(fis);
+            int i;
+            while((i = bis.read()) != -1)
+            {
+                System.out.print((char)i);
+            }
+            bis.close();
+            fis.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
+// SequentialInputStream example
+class InputStreamExample {    
+  public static void main(String args[])throws IOException{    
+   FileInputStream input1=new FileInputStream("D:\\testin.txt");    
+   FileInputStream input2=new FileInputStream("D:\\testout.txt");
+
+   SequenceInputStream inst=new SequenceInputStream(input1, input2);    
+   int j;    
+   while((j=inst.read())!=-1){    
+    System.out.print((char)j);    
+   }    
+   inst.close();    
+   input1.close();    
+   input2.close();    
+  }    
+}    
